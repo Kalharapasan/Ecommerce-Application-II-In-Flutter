39139c71e32912eaf9b80d849c0afd5c912232bf
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-01 14:10:47 +0530

    Auth Methods

--- ecom_ii/lib/core/services/supabase_service.dart
+++ ecom_ii/lib/core/services/supabase_service.dart
@@ -3,6 +3,19 @@ import 'package:ecom_ii/core/models/product.dart';
 import 'package:ecom_ii/core/models/user.dart' as app_user;
 
 class SupabaseService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+  Future<app_user.User?> getCurrentUser() async {
+    final session = _supabase.auth.currentSession;
+    if (session == null) return null;
 
-  
+    final response = await _supabase
+        .from('profiles')
+        .select()
+        .eq('id', session.user.id)
+        .single();
+
+    return app_user.User.fromJson(response);
+  }
+
+
 }
