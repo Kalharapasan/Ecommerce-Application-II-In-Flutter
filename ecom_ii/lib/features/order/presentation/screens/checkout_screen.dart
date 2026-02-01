b57c6008470c2842984b40f3e91c5efab2dbde42
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-01 14:28:06 +0530

    complete CheckoutScreen

--- ecom_ii/lib/features/order/presentation/screens/checkout_screen.dart
+++ ecom_ii/lib/features/order/presentation/screens/checkout_screen.dart
@@ -3,3 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:ecom_ii/features/cart/presentation/providers/cart_provider.dart';
 import 'package:ecom_ii/shared/widgets/custom_text_field.dart';
 import 'package:ecom_ii/shared/widgets/primary_button.dart';
+
+class CheckoutScreen extends StatefulWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
