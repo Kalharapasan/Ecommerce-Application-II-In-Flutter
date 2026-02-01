de59b1ae732abc77575820f67ef3800745ae93a7
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-01 14:39:45 +0530

    HomeScreen Complete

--- ecom_ii/lib/features/product/presentation/screens/home_screen.dart
+++ ecom_ii/lib/features/product/presentation/screens/home_screen.dart
@@ -4,3 +4,10 @@ import 'package:provider/provider.dart';
 import 'package:ecom_ii/core/models/product.dart';
 import 'package:ecom_ii/features/product/presentation/widgets/product_card.dart';
 import 'package:ecom_ii/features/cart/presentation/providers/cart_provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
