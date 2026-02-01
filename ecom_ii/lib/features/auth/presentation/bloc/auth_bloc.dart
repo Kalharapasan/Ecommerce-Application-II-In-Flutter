0be0dc27c57c4aed6bd5097ab660eb4f432af4ec
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-01 14:14:01 +0530

    Create AuthBloc class

--- ecom_ii/lib/features/auth/presentation/bloc/auth_bloc.dart
+++ ecom_ii/lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -4,3 +4,8 @@ import 'package:supabase_flutter/supabase_flutter.dart' hide AuthState;
 import 'package:ecom_ii/core/models/user.dart' as app_user;
 import 'auth_event.dart';
 import 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+
+  
+}
