f67aacdf1e08bd375b9a7ecf738184246afd6673
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-01 14:10:24 +0530

    Create SupabaseService class

--- ecom_ii/lib/core/services/supabase_service.dart
+++ ecom_ii/lib/core/services/supabase_service.dart
@@ -1,3 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ecom_ii/core/models/product.dart';
 import 'package:ecom_ii/core/models/user.dart' as app_user;
+
+class SupabaseService {
+
+  
+}
